2e0d540cd516e15439190d48f44e25fb56cdcdd5
Daniel Ancines <daniel.ancines@gmail.com>
2023-09-19 13:33:35 -0300

    Add project files.

---
+++ Maui.SetParentError/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Maui.Handlers;
+using Microsoft.Maui.Platform;
+using Windows.Win32;
+
+namespace Maui.SetParentError;
+
+public partial class MainPage : ContentPage
+{
+    int count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        Application.Current.OpenWindow(new MyWindow(new ContentPage()));
+    }
+}
+
+public class MyWindow : Window
+{
+    public MyWindow(Page page) : base(page)
+    {
+        
+    }
+}
+
+public class MyWindowHandler : WindowHandler
+{
+    protected override void ConnectHandler(Microsoft.UI.Xaml.Window platformView)
+    {
+        base.ConnectHandler(platformView);
+
+        var mainWindowHandle = (Application.Current.MainPage.Window.Handler.PlatformView as MauiWinUIWindow).GetWindowHandle();
+        var childWindowHandle = this.PlatformView.GetWindowHandle();
+
+        PInvoke.SetParent(new Windows.Win32.Foundation.HWND(childWindowHandle), new Windows.Win32.Foundation.HWND(mainWindowHandle));
+    }
+}
---
+++ Maui.SetParentError/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Maui.SetParentError
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
